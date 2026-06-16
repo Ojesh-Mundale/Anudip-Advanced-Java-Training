bf7443322e9d3663dcee31b78e2eb16d6225571a
Ojesh Mundale <ojeshmundale@gmail.com>
2026-06-16 14:42:05 +0530

    initial commit

---
+++ JavaDSA/src/com/dsa/DSA1.java
@@ -0,0 +1,14 @@
+package com.dsa;
+import java.util.*;
+
+public class DSA1 {
+   
+	public static void main(String[] args) {
+		
+		Vector<Integer> list =new Vector<Integer>();
+		System.out.println(list.capacity()); //10
+		
+		System.out.println(list.size());
+//		System.out.println(list.get(0));
+	}
+}
